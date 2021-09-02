4a87781015375c89b9b2770fdd0f80ae3c237933
seamussmith <student.141504@worcesterschools.net>
2021-09-02 17:47:10 -0400

    Use *superior* parenthesis on new line

--- src/com/App.java
+++ src/com/App.java
@@ -1,7 +1,9 @@
 package com;
 
-public class App {
-    public static void main(String[] args) {
+public class App 
+{
+    public static void main(String[] args) 
+    {
         System.out.println("Hello, World!");
     }
 }
